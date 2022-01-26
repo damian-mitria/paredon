25add453f73864880d4729349b6f857369959b67
damian-mitria <damian.mitria@gmail.com>
2022-01-26 17:17:24 -0300

    agregue loguearse, registrarte y logout, (con lo respectivo de firebase)

--- frontend/js/firebase.js
+++ frontend/js/firebase.js
@@ -1,25 +1,129 @@
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.3/firebase-app.js";
-  import { getFirestore, collection, addDoc, getDocs, query, orderBy } from "https://www.gstatic.com/firebasejs/9.6.3/firebase-firestore.js"
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.3/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.6.3/firebase-auth.js";
+import { getDatabase, set, ref, update } from "https://www.gstatic.com/firebasejs/9.6.3/firebase-database.js";
+import { getFirestore, collection, addDoc, getDocs, query, orderBy } from "https://www.gstatic.com/firebasejs/9.6.3/firebase-firestore.js"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyBhw5ifbn4wnklj2zmzevM1eM_lvHLbfck",
-    authDomain: "tuparedon.firebaseapp.com",
-    projectId: "tuparedon",
-    storageBucket: "tuparedon.appspot.com",
-    messagingSenderId: "151232886278",
-    appId: "1:151232886278:web:f9326c5d80f91289767587"
-  };
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBhw5ifbn4wnklj2zmzevM1eM_lvHLbfck",
+  authDomain: "tuparedon.firebaseapp.com",
+  projectId: "tuparedon",
+  storageBucket: "tuparedon.appspot.com",
+  messagingSenderId: "151232886278",
+  appId: "1:151232886278:web:f9326c5d80f91289767587"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
-  const db = getFirestore();
+const db = getFirestore();
 
-export const saveTask = (nombre, texto, fecha, d) => 
-  addDoc(collection(db,'tasks'), { nombre, texto, fecha, d });
+// Get a reference to the database service
+const database = getDatabase(app);
 
-export const getTasks = () => getDocs(query(collection(db,'tasks'),orderBy("d")));
+export const saveTask = (nombre, texto, fecha, d) =>
+  addDoc(collection(db, 'tasks'), { nombre, texto, fecha, d });
+
+export const getTasks = () => getDocs(query(collection(db, 'tasks'), orderBy("d")));
+
+
+
+const auth = getAuth(); // esto me sirve para todo lo que es logueo y registrarse
+
+
+// Registrarse
+const singupForm = document.querySelector("#singup-form");
+singupForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  //obtengo el email y password
+  const email = document.querySelector("#singup-email").value;
+  const password = document.querySelector("#singup-password").value;
+  //creo el usuario
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      
+      set(ref(database, 'users/' + user.uid), {   //<----seteo al usuario en la base de datos
+        email: email,
+        password: password
+      });
+      alert("Usuario registrado correctamente!");
+      singupForm.reset();
+      $('#singupModal').modal('hide');
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      alert(errorMessage);
+    });
+
+});
+
+//Loguearse
+const singInForm = document.querySelector("#login-form");
+singInForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  //obtengo el email y password
+  const email = document.querySelector("#login-email").value;
+  const password = document.querySelector("#login-password").value;
+  //creo el usuario
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+
+      const date = new Date(); // <----aca le estoy agregando cuando es su ultimo login
+      update(ref(database, 'users/' + user.uid), {
+        last_login: date
+      });
+
+      alert("Usted se ha logueado correctamente");
+      singInForm.reset();
+      $('#singinModal').modal('hide');
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      alert(errorMessage);
+    });
+
+});
+
+//Logout
+const logout = document.querySelector("#logout");
+logout.addEventListener('click', (e) => {
+  e.preventDefault();
+  signOut(auth).then(() => {
+    // Sign-out successful.
+    alert("Deslogueado. Gracias... vuelva pronto!")
+  }).catch((error) => {
+    const errorCode = error.code;
+      const errorMessage = error.message;
+      alert(errorMessage);
+  });
+
+});
+
+
+// Esto es para que todo el tiempo la app chequee si el usuario esta logueado
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/firebase.User
+    const uid = user.uid;
+    // ...
+    console.log("atroden");
+    $("#main").show();
+    $("#mensaje-footer").hide();
+  } else {
+    // User is signed out
+    // ...
+    console.log("aqui no ha entrado nadie hermanote");
+    $("#mensaje-footer").show();
+    $("#main").hide();
+  }
+});
--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,86 +1,91 @@
-import {saveTask, getTasks} from './firebase.js'
+import { saveTask, getTasks } from './firebase.js'
 
-
-
-$(async() => {
+$(async () => {
     const querySnapShots = await getTasks();
     $("#spinner").hide();
     querySnapShots.forEach(doc => {
         let objeto = doc.data();
         $(`<div class='ladrillo' data-bs-toggle='modal' data-bs-target='#myModal'><p class='contenido-nombre'>${objeto.nombre}</p><p class='contenido-texto'>${objeto.texto}</p></div>`)
-        .prependTo("#pared")
-        .on('click', function(event){
-            $("#modalTitle").text($(event.currentTarget).children(".contenido-nombre").get(0).innerHTML);
-            $("#divMensaje").text($(event.currentTarget).children(".contenido-texto").get(0).innerHTML);
-            $("#divFecha").text(objeto.fecha);  
-        });
+            .prependTo("#pared")
+            .on('click', function (event) {
+                $("#modalTitle").text($(event.currentTarget).children(".contenido-nombre").get(0).innerHTML);
+                $("#divMensaje").text($(event.currentTarget).children(".contenido-texto").get(0).innerHTML);
+                $("#divFecha").text(objeto.fecha);
+            });
         $(".contenido-texto").hide();
     });
-    
+
     contarLadrillos();
 
 });
 
-$(function(){
+$(function () {
 
-    
-
-    $("#boton-enviar").on('click', function(){
+    $("#boton-enviar").on('click', function () {
         /*obtengo datos del html*/
         let nombre = $("#nombre").val();
         let texto = $("#texto").val();
         let d = new Date();
-        let month = d.getMonth()+1;
+        let month = d.getMonth() + 1;
         let day = d.getDate();
-        let fecha = (day<10 ? '0' : '') + day + '/' + (month<10 ? '0' : '') + month + '/' + d.getFullYear();
+        let fecha = (day < 10 ? '0' : '') + day + '/' + (month < 10 ? '0' : '') + month + '/' + d.getFullYear();
 
         $("#mensaje-nombre").empty();
         $("#mensaje-texto").empty();
         $("#nombre, #texto").removeClass("error");
 
-        if(nombre===""){
+        if (nombre === "") {
             $("#nombre").addClass("error");
             $("#mensaje-nombre").append("<p>El nombre es obligatorio</p>");
         }
 
-        if(texto===""){
+        if (texto === "") {
             $("#texto").addClass("error");
             $("#mensaje-texto").append("<p>El texto es obligatorio</p>");
-            if(nombre.length >=12){
+            if (nombre.length >= 12) {
                 $("#mensaje-nombre").append("<p>El nombre no puede tener más de 11 caracteres</p>");
                 $("#nombre").addClass("error");
             }
         }
 
-        if(nombre!=="" && texto!==""){
-            if(nombre.length >=12){
+        if (nombre !== "" && texto !== "") {
+            if (nombre.length >= 12) {
                 $("#mensaje-nombre").append("<p>El nombre no puede tener mas de 11 caracteres</p>");
                 $("#nombre").addClass("error");
-       
-            }else{
+
+            } else {
                 $(`<div class='ladrillo' data-bs-toggle='modal' data-bs-target='#myModal'><p class='contenido-nombre'>${nombre}</p><p class='contenido-texto'>${texto}</p></div>`)
-                .prependTo("#pared")
-                .on('click', function(event){
-                    $("#modalTitle").text($(event.currentTarget).children(".contenido-nombre").get(0).innerHTML);
-                    $("#divMensaje").text($(event.currentTarget).children(".contenido-texto").get(0).innerHTML);
-                    $("#divFecha").text(fecha);  
-                });
+                    .prependTo("#pared")
+                    .on('click', function (event) {
+                        $("#modalTitle").text($(event.currentTarget).children(".contenido-nombre").get(0).innerHTML);
+                        $("#divMensaje").text($(event.currentTarget).children(".contenido-texto").get(0).innerHTML);
+                        $("#divFecha").text(fecha);
+                    });
                 $(".contenido-texto").hide();
                 saveTask(nombre, texto, fecha, d); //guardo todos los datos para el firebase
-               
+
                 $("#nombre").val(""); //reseteo los input
                 $("#texto").val("");
 
                 contarLadrillos();
-                }  
+            }
         }
     });
 
+    /*const singupForm = document.querySelector("#singup-form");
+    singupForm.addEventListener('submit', (e) =>{
+        e.preventDefault();
 
-   
+        const email = document.querySelector("#singup-email").value;
+        const password = document.querySelector("#singup-password").value;
+
+        
+        console.log(email,password);
+    });*/
+
 });
 
-function contarLadrillos(){
+function contarLadrillos() {
     let contadorDeLadrillos = document.getElementsByClassName("contenido-nombre").length;
     $("#contadorDeLadrillos").empty();
     $("#contadorDeLadrillos").append(`<p>La cantidad de ladrillos por el momento es: ${contadorDeLadrillos}</p>`);
